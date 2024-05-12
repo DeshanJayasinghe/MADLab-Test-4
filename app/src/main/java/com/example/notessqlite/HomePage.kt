d4147bcfdad6c337c7c4ba6dbf229bfc384c7dfe
IT22106124MDSJayasinghe <160699724+DeshanJayasinghe123@users.noreply.github.com>
2024-05-12 22:18:54 +0530

    Final commit

---
+++ app/src/main/java/com/example/notessqlite/HomePage.kt
@@ -0,0 +1,31 @@
+package com.example.notessqlite
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val getStart: Button =findViewById(R.id.button2)
+
+        getStart.setOnClickListener{
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/notessqlite/LaunchPage.kt
@@ -0,0 +1,30 @@
+package com.example.notessqlite
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LaunchPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_launch_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val getStart: Button =findViewById(R.id.button11)
+
+        getStart.setOnClickListener{
+            val intent= Intent(this,HomePage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
